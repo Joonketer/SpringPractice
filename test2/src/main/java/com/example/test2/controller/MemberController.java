ba1a7208224931a1fe81cff3f84d65c1ddac7729
DESKTOP-DOGVPUB\SSAFY <hoilday5303@naver.com>
2023-07-12 10:31:59 +0900

    crud 생성

--- test2/src/main/java/com/example/test2/controller/MemberController.java
+++ test2/src/main/java/com/example/test2/controller/MemberController.java
@@ -4,14 +4,17 @@ import com.example.test2.dto.MemberDTO;
 import com.example.test2.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
+@RequiredArgsConstructor    // MemberService에 대한 멤버를 사용 가능
 public class MemberController {
 
     // 생성자 주입
@@ -49,4 +52,28 @@ public class MemberController {
             return "login";
         }
     }
+
+    @GetMapping("/member/")     // 전체 맴버 가져오기
+    public String findAll(Model model){
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 model 사용
+        model.addAttribute("memberList",memberDTOList);
+        return "list";
+    }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        // login 처럼 return 값에 따라 분류 할 수 있음
+        model.addAttribute("member", memberDTO);
+        return "detail";
+    }
+
+    @GetMapping("/member/delete/{id}") // /member/{id}로 할 수 있도록 공부
+    public String deleteById(@PathVariable Long id){
+        memberService.deleteByid(id);
+
+        return "redirect:/member/"; // list 로 쓰면 껍데기만 보여짐
+    }
+
 }
